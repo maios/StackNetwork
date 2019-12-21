bb090526aa3d4e6aeb8ba9d6d0555cdf0bd331f1
maios <mmai.iossg@gmail.com>
2019-12-21 11:46:59 +0200

    Initial commit

---
+++ StackNetwork/StackNetwork.h
@@ -0,0 +1,19 @@
+//
+//  StackNetwork.h
+//  StackNetwork
+//
+//  Created by Mai Mai on 12/21/19.
+//  Copyright © 2019 maimai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StackNetwork.
+FOUNDATION_EXPORT double StackNetworkVersionNumber;
+
+//! Project version string for StackNetwork.
+FOUNDATION_EXPORT const unsigned char StackNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StackNetwork/PublicHeader.h>
+
+
