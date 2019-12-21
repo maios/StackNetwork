bfe798fd3b3dbfc12e9d5afc77c9bcee04fee2a6
maios <mmai.iossg@gmail.com>
2019-12-21 12:25:40 +0200

    Structure projects

---
+++ StackNetwork/Source/HTTPMethod.swift
@@ -0,0 +1,9 @@
+//
+//  HTTPMethod.swift
+//  StackNetwork
+//
+//  Created by Mai Mai on 12/21/19.
+//  Copyright © 2019 maimai. All rights reserved.
+//
+
+import Foundation
---
+++ StackNetwork/Source/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  StackNetwork
+//
+//  Created by Mai Mai on 12/21/19.
+//  Copyright © 2019 maimai. All rights reserved.
+//
+
+import Foundation
